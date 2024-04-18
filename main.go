e79c97b9bae0723587aea9ac782a01db8a28b4a6
judeosbert <judeosby@gmail.com>
2024-04-18 13:29:57 +0530

    log body

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+
 	"github.com/gin-gonic/gin"
 	"github.com/judeosbert/bus_tracker_bot/telegram"
 )
 
-func main(){
+func main() {
 	r := gin.Default()
-	r.POST("/",func(ctx *gin.Context) {
+	r.POST("/", func(ctx *gin.Context) {
+		body, _ := io.ReadAll(ctx.Request.Body)
+		fmt.Sprintf("incoming body %s",string(body))
+
+		ctx.Request.Body = io.NopCloser(bytes.NewReader(body))
 		telegram.HandleIncomingMessage(ctx)
 	})
 	r.Run()
