d83cdf6242293177a3b20538135329fbe6616759
Jude Osbert K <judeosby@gmail.com>
2024-05-12 09:25:04 +0530

    add deploy env variable

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -16,15 +17,15 @@ import (
 )
 
 func main() {
-	stateSaver := state.NewStateSaver()	
+	stateSaver := state.NewStateSaver()
 	botToken := os.Getenv("TELEGO_BOT_TOKEN")
-	
+
 	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
 	if err != nil {
 		panic(err)
 	}
 	bot.SetMyCommands(&telego.SetMyCommandsParams{
-		Commands:     []telego.BotCommand{
+		Commands: []telego.BotCommand{
 			{
 				Command:     "/start",
 				Description: "Copy paste ticket",
@@ -35,19 +36,41 @@ func main() {
 			},
 		},
 	})
-	admin := admin.NewAdminUtils(bot,stateSaver)
-	botEngine := botengine.NewBotEnginer(stateSaver,admin)
+	admin := admin.NewAdminUtils(bot, stateSaver)
+	botEngine := botengine.NewBotEnginer(stateSaver, admin)
 
-	updates, err := bot.UpdatesViaLongPolling(
-		&telego.GetUpdatesParams{
-			Offset:  0, // Will be automatically updated by UpdatesViaLongPolling
-			Timeout: 8, // Can be set instead of using WithLongPollingUpdateInterval (default, recommended way)
-		}, telego.WithLongPollingUpdateInterval(time.Second*0), telego.WithLongPollingRetryTimeout(time.Second*8), telego.WithLongPollingBuffer(100))
-	if err != nil {
-		panic(err)
+	var env = os.Getenv("MODE")
+	var updates <-chan telego.Update
+	if env == "dev" {
+		updates, err = bot.UpdatesViaLongPolling(
+			&telego.GetUpdatesParams{
+				Offset:  0, // Will be automatically updated by UpdatesViaLongPolling
+				Timeout: 8, // Can be set instead of using WithLongPollingUpdateInterval (default, recommended way)
+			}, telego.WithLongPollingUpdateInterval(time.Second*0), telego.WithLongPollingRetryTimeout(time.Second*8), telego.WithLongPollingBuffer(100))
+		if err != nil {
+			panic(err)
+		}
+
+		defer bot.StopLongPolling()
+	} else {
+		_ = bot.SetWebhook(&telego.SetWebhookParams{
+			URL: "https://telegrambustracker-production.up.railway.app/bot" + bot.Token(),
+		})
+
+		info, _ := bot.GetWebhookInfo()
+		fmt.Printf("Webhook Info: %+v\n", info)
+
+		updates, _ = bot.UpdatesViaWebhook("/bot" + bot.Token())
+
+		go func() {
+			_ = bot.StartWebhook("0.0.0.0:443")
+		}()
+
+		// Stop reviving updates from update channel and shutdown webhook server
+		defer func() {
+			_ = bot.StopWebhook()
+		}()
 	}
-
-	defer bot.StopLongPolling()
 
 	bh, err := th.NewBotHandler(bot, updates)
 	if err != nil {
@@ -56,33 +79,32 @@ func main() {
 	defer bh.Stop()
 
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		addpnr.Handler(bot,update,stateSaver)
-	},addpnr.Predicate)
-	
+		addpnr.Handler(bot, update, stateSaver)
+	}, addpnr.Predicate)
+
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		start.Handler(bot,update,stateSaver)
-	},start.Predicate)
-	
+		start.Handler(bot, update, stateSaver)
+	}, start.Predicate)
+
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		deletetrip.Handler(bot,update,stateSaver)
-	},deletetrip.Predicate)
+		deletetrip.Handler(bot, update, stateSaver)
+	}, deletetrip.Predicate)
 
 	bh.HandleMessage(func(bot *telego.Bot, message telego.Message) {
 		botEngine.PostUpdate(message)
 	})
-	
-	bh.HandleCallbackQuery(func(bot *telego.Bot, query telego.CallbackQuery) {
-		
-	},th.AnyCallbackQuery())
 
-	
-	go func(){
-		for msg := range botEngine.OutChan(){
-			log.Printf("Sending message out from channel %+v\n",msg)
+	bh.HandleCallbackQuery(func(bot *telego.Bot, query telego.CallbackQuery) {
+
+	}, th.AnyCallbackQuery())
+
+	go func() {
+		for msg := range botEngine.OutChan() {
+			log.Printf("Sending message out from channel %+v\n", msg)
 			bot.SendMessage(&msg)
 		}
 	}()
-	go func(){
+	go func() {
 		botEngine.Start()
 	}()
 	bh.Start()
